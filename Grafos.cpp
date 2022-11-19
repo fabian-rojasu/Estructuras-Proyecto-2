b8419e92478180b48d0b84ba3f5ce7339000e15b
Fabián Rojas <fabianrojas@MacBook-Pro-de-Fabian.local>
2022-11-18 19:57:25 -0600

    CargarDatos

--- Grafos.cpp
+++ Grafos.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
-
+//Esta estructura sera la que se utilice para representar los lugares que seran visitados 
 struct Vertice{
+    //Atributos de la estructura
     string nombre;
-
     struct Vertice * sigV;//para enlazar todos los vertices en una lista
     struct Arco *subListaArcos;//representa los arcos que salen de ese vertice
-
     bool visitado;
+    //Constructor
     Vertice (string nom){
             nombre = nom;
             sigV = NULL;
@@ -17,10 +18,13 @@ struct Vertice{
     }
 };
 
+//Esta estructura sera la que se utilice para representar la conexion que que habra entre los diferentes lugares
 struct Arco{
-    int distancia;
+    //Atributos de la estructura
+    int distancia;//Medida que tendran que recorrer de un vertice a otro
     string destino;
     struct Arco *sigA;//siguiente arco.
+    //Constructor
     Arco(int dis, string des){
             distancia = dis;
             destino = des;
@@ -28,51 +32,106 @@ struct Arco{
     }
 };
 
+//Esta estructura sera la que se utilice para representar a los individuos que van a recorrer los diferentes lugares
 struct Personas{
+    //Atributos de la estructura
     string nombre;
-    string cedula;
+    string cedula;//Indentificador
+    struct Vertice * inicio;//Este sera el punto de partida de la persona respectiva 
+    string formaAvanzar; //Aqui se determinara la forma en la que la persona respectiva se transaladara por los diferentes lugares
     Personas * ant;
     Personas * sig;
-    Personas(string nom,string ced){
+    //Constructor
+    Personas(string nom,string ced, Vertice * ini, string FAva){
         nombre = nom;
         cedula=ced;
+        inicio=ini;
+        formaAvanzar=FAva;
         ant = NULL;
         sig = NULL;
     }
 
 } * nodoPersona;
 
-//Vertice
+//Metodos Vertice
 
-struct Vertice *grafo;// el apuntador inicial del grafo
+struct Vertice *grafo1;// el apuntador inicial del grafo
+struct Vertice *grafo2;// el apuntador inicial del grafo
 
 
-void insertarVertice(string nom){
-            struct Vertice *nuevoVertice = new Vertice(nom);
+void insertarVertice(string nom,Vertice*graf){
+    struct Vertice *nuevoVertice = new Vertice(nom);
 
-            nuevoVertice->sigV = grafo;
-            grafo = nuevoVertice;
+    nuevoVertice->sigV = graf;
+    graf = nuevoVertice;
 }
 
 
-struct Vertice *   buscarVertice(string origen){
-        struct Vertice *tempV = grafo;
-        while(tempV != NULL){
-            if(tempV->nombre == origen)
-                return tempV;
+struct Vertice *   buscarVertice(string origen,Vertice*graf){
+    struct Vertice *tempV = graf;
+    while(tempV != NULL){
+        if(tempV->nombre == origen)
+            return tempV;
 
-            tempV = tempV->sigV;
+        tempV = tempV->sigV;
+    }
+return NULL;//no lo encontro
+}
+
+void modificarVertice(Vertice*graf){
+    string nom;
+    printf("Nombre del vertice:  ");
+    cin>> nom;
+    struct Vertice *tempV = buscarVertice(nom,graf);
+    if (tempV == NULL){
+        printf("Persona no encontrado");
+    }else{
+        printf("\n\nNombre nuevo:  ");
+        cin>> nom; 
+        tempV->nombre = nom;
+    }
+}
+
+//Metodo Borrar: selecciona al nodo que se desea eliminar por medio de su respectivo buscar
+void borrarVertice(string nom,Vertice*graf){
+    Vertice* buscado = buscarVertice(nom,graf);
+    if(buscado == NULL){
+        cout<<"\n vertice no encontrada....\n";
+        return;
+    }
+    else{
+        if(buscado == graf){//si el buscado es el primero
+                graf = graf->sigV;
         }
-    return NULL;//no lo encontro
+        else{//borra en medio o al final
+            Vertice * temp =graf->sigV; /*1    2   3*/
+            Vertice * ant =graf;
+            while ((temp!= NULL) && (temp!=buscado))
+            {
+                ant = temp;
+                temp = temp->sigV;
+            }
+            if(temp==buscado){
+                if (temp->sigV==NULL)
+                {
+                    ant->sigV=NULL;
+                }else{
+                    ant->sigV=temp->sigV;  
+
+                }
+            }
+            
+        }
+    }
 }
 
 
-//Arco
+//Metodos Arco
 
-void insertarArco(string origen, int dis, string des){
+void insertarArco(string origen, int dis, string des,Vertice*graf){
 
-        struct Vertice *vOrigen = buscarVertice(origen);
-        struct Vertice *vDestino = buscarVertice(des);
+        struct Vertice *vOrigen = buscarVertice(origen,graf);
+        struct Vertice *vDestino = buscarVertice(des,graf);
 
         if(vOrigen == NULL){
             cout<<"\nNo se encuentra el origen.";
@@ -90,7 +149,7 @@ void insertarArco(string origen, int dis, string des){
 
 }
 
-//Persona
+//Metodos Persona
 
 //Metodo Buscar: inicia la busqueda a partir del indentificador unico de la persona que seria la cedula y retorna dicha persona 
 Personas* buscarPersona(string ced){
@@ -114,10 +173,10 @@ Personas* buscarPersona(string ced){
 }
 
 //Metodo Insertar: a partir del nodo de personas agrega a las personas con le metodo de una lista doble con insercion ordenado por nombre
-Personas * insertarPersona(string nom , string ced ,Personas *lista){
+Personas * insertarPersona(string nom , string ced , Vertice* inicio,string forma, Personas *lista){
 
-    if(buscarPersona(c)==NULL){
-        Personas*nuevo = new Personas(nom,ced);
+    if(buscarPersona(ced)==NULL){
+        Personas*nuevo = new Personas(nom,ced,inicio,forma);
 
         if(nodoPersona== NULL)
             lista = nuevo;
@@ -197,11 +256,70 @@ void borrarPersona(string ced){
 }
 
 void cargarDatos(){
+    //GRAFO1
+    insertarVertice("Sabanilla",grafo1);
+    insertarVertice("San Pedro",grafo1);
+    insertarVertice("Curri",grafo1);
+    insertarVertice("Tibas",grafo1);
+    insertarVertice("Escalante",grafo1);
 
+    insertarArco("Sabanilla",rand()%21,"San Pedro",grafo1);
+    insertarArco("Sabanilla",rand()%21,"Curri",grafo1);
+    insertarArco("Sabanilla",rand()%21,"Tibas",grafo1);
+    insertarArco("San Pedro",rand()%21,"Curri",grafo1);
+    insertarArco("San Pedro",rand()%21,"Escalante",grafo1);
+    insertarArco("Curri",rand()%21,"Curri",grafo1);
+    insertarArco("Tibas",rand()%21,"Escalante",grafo1);
+    insertarArco("Tibas",rand()%21,"San Pedro",grafo1);
+    insertarArco("Escalante",rand()%21,"Sabanilla",grafo1);
+    insertarArco("Escalante",rand()%21,"San Pedro",grafo1);
+    
+
+    
+
+    //GRAFO2
+    insertarVertice("Lourdes",grafo2);
+    insertarVertice("Campo",grafo2);
+    insertarVertice("Gamonales",grafo2);
+    insertarVertice("San Antonio",grafo2);
+    insertarVertice("San Roque",grafo2);
+    insertarVertice("San Vicente",grafo2);
+    insertarVertice("Cedral",grafo2);
+
+    insertarArco("Lourdes",rand()%21,"Gamonales",grafo2);
+    insertarArco("Lourdes",rand()%21,"San Antonio",grafo2);
+    insertarArco("Lourdes",rand()%21,"San Vicente",grafo2);
+    insertarArco("Campo",rand()%21,"Lourdes",grafo2);
+    insertarArco("Campo",rand()%21,"Campo",grafo2);
+    insertarArco("Campo",rand()%21,"Gamonales",grafo2);
+    insertarArco("Gamonales",rand()%21,"San Antonio",grafo2);
+    insertarArco("Gamonales",rand()%21,"Cedral",grafo2);
+    insertarArco("Gamonales",rand()%21,"San Roque",grafo2);
+    insertarArco("San Roque",rand()%21,"San Roque",grafo2);
+    insertarArco("San Roque",rand()%21,"Campo",grafo2);
+    insertarArco("San Roque",rand()%21,"San Antonio",grafo2);
+    insertarArco("San Roque",rand()%21,"Cedral",grafo2);
+    insertarArco("San Roque",rand()%21,"Cedral",grafo2);
+    insertarArco("San Antonio",rand()%21,"Campo",grafo2);
+    insertarArco("San Antonio",rand()%21,"San Roque",grafo2);
+    insertarArco("San Antonio",rand()%21,"Cedral",grafo2);
+    insertarArco("Cedral",rand()%21,"Cedral",grafo2);
+    insertarArco("Cedral",rand()%21,"Lourdes",grafo2);
+    insertarArco("Cedral",rand()%21,"San Vicente",grafo2);
+    insertarArco("San Vicente",rand()%21,"San Vicente",grafo2);
+    insertarArco("San Vicente",rand()%21,"Lourdes",grafo2);
+    insertarArco("San Vicente",rand()%21,"Campo",grafo2);
+    insertarArco("San Vicente",rand()%21,"San Antonio",grafo2);
+    insertarArco("San Vicente",rand()%21,"Cedral",grafo2);
+
+    //Personas
+    
 
 }
 
+//Metodo principal donde se ejecutara todo
 int main(){
+    cout<<"*****Sistema de caminatas*****";
     cargarDatos();
 
 }
